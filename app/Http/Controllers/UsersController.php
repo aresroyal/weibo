7ce8e25fa252448bc316074670abc7a2c3a83505
vagrant <vagrant@homestead>
2019-08-02 07:41:36 +0000

    粉丝数据表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,6 +39,32 @@ class UsersController extends Controller
           return view('users.show', compact('user', 'statuses'));
   }
 
+  public function edit(User $user)
+{
+    $this->authorize('update', $user);
+    return view('users.edit', compact('user'));
+}
+
+public function update(User $user, Request $request)
+{
+    $this->authorize('update', $user);
+    $this->validate($request, [
+        'name' => 'required|max:50',
+        'password' => 'nullable|confirmed|min:6'
+    ]);
+
+    $data = [];
+    $data['name'] = $request->name;
+    if ($request->password) {
+        $data['password'] = bcrypt($request->password);
+    }
+    $user->update($data);
+
+    session()->flash('success', '个人资料更新成功！');
+
+    return redirect()->route('users.show', $user->id);
+}
+
   public function store(Request $request)
   {
       $this->validate($request, [
